06154cf96128d546635829b2e80a51449d521944
Sascha Andeweg <sascha@designed.irish>
2020-04-18 11:29:43 +0000

    commit of changes in run.py andnew chat.html file to render the chat page with html

--- run.py
+++ run.py
@@ -34,7 +34,7 @@ def index():
 @app.route("/<username>")
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+    return render_template("chat.html", username = username, chat_messages = messages)
 
 
 @app.route("/<username>/<message>")
